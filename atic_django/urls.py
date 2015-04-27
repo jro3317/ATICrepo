b8d45e0a67bc140f527640f0d00edbc312a553d6
Chris Hedrick <christopher.m.hedrick@gmail.com>
2015-04-27 05:53:49 -0400

    convert the rest of the .tpl's, edit urls to render templates

--- atic_django/urls.py
+++ atic_django/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^about/$', TemplateView.as_view(template_name="info.html")),
+    url(r'^projects/$', TemplateView.as_view(template_name="projects.html")),
+    url(r'^contact/$', TemplateView.as_view(template_name="contact.html"))
 ]
